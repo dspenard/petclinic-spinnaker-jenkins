ef1c39a28b90e4d969ef4ae8ce6e4b70e5792e6c
David Spenard <dspenard@users.noreply.github.com>
2020-10-02 14:09:44 -0400

    Update install_jenkins.sh

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -21,9 +21,15 @@ yum install docker -y
 sudo chkconfig docker on
 
 echo "Install Jenkins"
+
+#if wget fails, check permission: e.g. sudo chmod 777 /etc/yum.repos.d
+#also might need double-quotes around URL
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+
+#if issues with key then do: yum install -y jenkins --nogpgcheck
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 yum install -y jenkins
+
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
